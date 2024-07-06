91cd1ecd6bfb692f22b6a78eef6df963278c25b3
lapla-cogito <kakikei20021108@gmail.com>
2024-07-06 11:58:31 +0900

    add license

--- components/particles.jsx
+++ components/particles.jsx
@@ -1,3 +1,12 @@
+/* -----------------------------------------------
+/* Author : Vincent Garreau  - vincentgarreau.com
+/* MIT license: http://opensource.org/licenses/MIT
+/* Demo / Generator : vincentgarreau.com/particles.js
+/* GitHub : github.com/VincentGarreau/particles.js
+/* How to use? : Check the GitHub README
+/* v2.0.0
+/* ----------------------------------------------- */
+
 import React, { useEffect } from 'react';
 import TypeNefront from '@/typeNefront';
 
